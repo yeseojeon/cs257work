da5868013b8ad74576464c571c2a19d9f195027f
Yeseo Jeon <yeseojeon@Yeseos-MacBook-Pro.local>
2024-01-30 16:17:14 -0600

    updated python file

--- pythonlab/createtables.py
+++ pythonlab/createtables.py
@@ -1,11 +1,11 @@
-DROP TABLE states;
+DROP TABLE IF EXISTS states;
 CREATE TABLE states (
   state text,
   abbreviation text
 );
 
-DROP TABLE us-cities;
-CREATE TABLE us-cities (
+DROP TABLE IF EXISTS us_cities;
+CREATE TABLE us_cities (
   city text,
   state text,
   population int,
