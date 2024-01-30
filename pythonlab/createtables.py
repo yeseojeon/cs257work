cf98740b371a0e232433f281574e2d9c58c7fabd
Yeseo Jeon <yeseojeon@Yeseos-MacBook-Pro.local>
2024-01-30 16:48:43 -0600

    updated python file

--- pythonlab/createtables.py
+++ pythonlab/createtables.py
@@ -16,17 +16,19 @@ def test_connection():
         else:
             print("Not Connected.")
 
-        # SQL statements to drop and create the "states" table
         drop_states_sql = """ DROP TABLE IF EXISTS states; """
         create_states_sql = """
-            CREATE TABLE states ( state text, abbreviation text);
+            CREATE TABLE states (state text, abbreviation text);
         """
 
-        # SQL statements to drop and create the "us_cities" table
         drop_cities_sql = """ DROP TABLE IF EXISTS us_cities; """
         create_cities_sql = """
-            CREATE TABLE us_cities ( city text, state text, population int, 
-            lat real, lon real
+            CREATE TABLE us_cities (
+                city text,
+                state text,
+                population int,
+                lat real,
+                lon real
             );
         """
 
@@ -38,6 +40,5 @@ def test_connection():
         cur.execute(sql.SQL(create_cities_sql))
 
         conn.commit()
-
 
 test_connection()
