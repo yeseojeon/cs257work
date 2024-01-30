54a50215d012dc9b92c0cb1caf3614a6c8d5753d
Yeseo Jeon <yeseojeon@Yeseos-MacBook-Pro.local>
2024-01-30 15:43:50 -0600

    Added python file and csv files

--- pythonlab/createtables.py
+++ pythonlab/createtables.py
@@ -1,1 +1,14 @@
-.
+DROP TABLE IF EXISTS states;
+CREATE TABLE states (
+  state text,
+  abbreviation text
+);
+
+DROP TABLE IF EXISTS us cities;
+CREATE TABLE us cities (
+  city text,
+  state text,
+  population int,
+  lat real,
+  lon real
+);
