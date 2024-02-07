786052846901a4e780602836c80bfee5dcafc3d3
Yeseo Jeon <yeseojeon@Yeseos-MacBook-Pro.local>
2024-02-07 09:16:27 -0600

    flasklab

--- flasklab/flask-test.py
+++ flasklab/flask-test.py
@@ -18,7 +18,7 @@ def my_color(word1):
 
 @app.route('/add/num1/num2')
 def my_add(num1, num2):
-    addition = num1 + num2;
+    addition = num1 + num2
     return addition
 
 if __name__ == '__main__':
