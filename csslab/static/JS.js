ab32dad779e0e91b72029db6f45d926193b785c1
Yeseo Jeon <yeseojeon@Yeseos-MacBook-Pro.local>
2024-02-20 16:51:53 -0600

    csslab materials

--- csslab/static/JS.js
+++ csslab/static/JS.js
@@ -1,10 +1,13 @@
-function toPink() {
+function surprise() {
   the_surprise = document.getElementById("BodyColor");
-  the_surprise.style.color = "pink";
+  the_surprise.style.color = "PaleVioletRed";
+  the_surprise.style.fontSize = "60px";
+  the_surprise.style.fontWeight = "bold";
 }
 
 function changeSizeAndColor() {
   console.log("Mouse over title!");
   the_Title = document.getElementById("Move");
-  the_Title.style.fontSize = "40px";
+  the_Title.style.fontSize = "60px";
+  the_Title.style.color = "RoyalBlue";
 }
