e04d216ab1afb32cea359c1cf8586102b71b272c
Yeseo Jeon <yeseojeon@Yeseos-MacBook-Pro.local>
2024-02-20 16:43:27 -0600

    csslab materials

--- csslab/static/JS.js
+++ csslab/static/JS.js
@@ -4,6 +4,7 @@ function toPink() {
 }
 
 function changeSizeAndColor() {
+  console.log("Mouse over title!");
   the_Title = document.getElementById("Move");
   the_Title.style.fontSize = "40px";
 }
